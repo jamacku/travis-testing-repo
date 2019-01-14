60aa4050ec98c5f25dcfc8f765e2d88879a9707f
Jan Macku <jamacku@redhat.com>
2019-01-14 17:07:06 +0100

    Add check for older version

--- ci/check.sh
+++ ci/check.sh
@@ -1,1 +1,15 @@
+while read DATA; do 
+  OLD=$DATA
+done < <(git rev-list --all --max-count=2)
+
+echo $OLD
+
+# get paths to changed shell files
+
+#Check for New commit
 shellcheck $PWD/script.sh
+
+#Check for prev commit
+git checkout $OLD
+shellcheck $PWD/script.sh
+
