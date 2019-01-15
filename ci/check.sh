c45ec71dd411a77e5cae1cee93c1cdbb2d12e688
Jan Macku <jamacku@redhat.com>
2019-01-15 09:49:23 +0100

    Add text output for test

--- ci/check.sh
+++ ci/check.sh
@@ -1,15 +1,26 @@
-while read DATA; do 
+while read -r DATA; do 
   OLD=$DATA
 done < <(git rev-list --all --max-count=2)
 
-echo $OLD
+echo "$OLD"
 
 # get paths to changed shell files
 
 #Check for New commit
-shellcheck $PWD/script.sh
+shellcheck "$PWD"/script.sh
+
+if [ $? -eq 0 ]; then
+  echo "NEW BUILD is OK!"
+else
+  echo "NEW BUIL is WRONG!"
+fi
 
 #Check for prev commit
-git checkout $OLD
-shellcheck $PWD/script.sh
+git checkout "$OLD"
+shellcheck "$PWD"/script.sh
 
+if [ $? -eq 0 ]; then
+  echo "NEW BUILD is OK!"
+else
+  echo "NEW BUIL is WRONG!"
+fi
