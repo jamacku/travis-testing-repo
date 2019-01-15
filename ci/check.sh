2de914cdd5270d7b98c53c5ff4ecea7bccb6ca58
Jan Macku <jamacku@redhat.com>
2019-01-15 16:44:56 +0100

    Better detection of revisions

--- ci/check.sh
+++ ci/check.sh
@@ -1,28 +1,37 @@
-while read -r DATA; do 
-  OLD=$DATA
-done < <(git rev-list --all --max-count=2)
+#!/bin/sh
+
+REV=($(git log --name-only --oneline --max-count=2 | awk '{ print $1 }' | awk '/[0-9a-f]{7}/ { print }'))
+
+# Revisions are in this order [New, Old]
+echo ${REV[@]}
 
 # get paths to changed shell files
 # git log --name-only --oneline --max-count=2
+# git log --name-only --oneline --max-count=2 | awk '/[0-9a-f]{7} / { print $1 }' 
+# git log --name-only --oneline --max-count=2 | awk '/[0-9a-f]{7} / { print $1 } /sh/ { print $1 }'
+# git log --name-only --oneline --max-count=2 | awk '{ print $1 }' | awk '/[0-9a-f]{7}/ { print } /sh/ { print }'
 
-#Check for New commit
+#------------#
+# ShellCheck #        
+#------------#
+
 shellcheck "$PWD"/script.sh
 
-if [ $? -eq 0 ]; then
-  echo "NEW BUILD is OK!"
-else
-  echo "NEW BUILD is WRONG!"
-fi
+#if [ $? -eq 0 ]; then
+#  echo "NEW BUILD is OK!"
+#else
+#  echo "NEW BUILD is WRONG!"
+#fi
 
 #Check for prev commit
-git checkout "$OLD"
+git checkout ${REV[1]}
 shellcheck "$PWD"/script.sh
 
-if [ $? -eq 0 ]; then
-  echo "OLD BUILD is OK!"
-else
-  echo "OLD BUILD is WRONG!"
-fi
+#if [ $? -eq 0 ]; then
+#  echo "OLD BUILD is OK!"
+#else
+#  echo "OLD BUILD is WRONG!"
+#fi
 
 #Pass final exit status for build
 #exit 
