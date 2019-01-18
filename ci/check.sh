d86615eb7dbe473766ec3528934484b38fe0a163
Jan Macku <jamacku@redhat.com>
2019-01-18 14:56:42 +0100

    Add file type checking

--- ci/check.sh
+++ ci/check.sh
@@ -7,36 +7,45 @@ rlist=($(git log --name-only --oneline --max-count=2 | awk '{ print $1 }' | awk 
 echo "Building from revisions:"
 echo "Old revision: ${rlist[1]}"
 echo "New revision: ${rlist[0]}"
+echo "------------"
 
 # Maybe better would be just take all rows instead of first one
 flist=($(git log --name-only --oneline --max-count=1 | awk '{ print $1 }' | awk '/[^'${rlist[0]}']/ { print }'))
 echo "List of changed files since last revision: "
 printf '%s\n' "${flist[@]}"
+echo "------------"
+
+#------------#
+#    NEW     #
+#------------#
 
 # Check for shell script files
+echo "List of shell files based on .script-list.txt: "
 cat "$PWD"/.script-list.txt
+echo "------------"
 
 shflist=()
-for file in "${flist[@]}"
-do
+for file in "${flist[@]}"; do
   grep -Fxq "$file" .script-list.txt && shflist+=("$file")
 done
 
 echo "List of shell files: "
 printf '%s\n' "${shflist[@]}"
+echo "------------"
 
-#------------#
-# ShellCheck #        
-#------------#
-
-shellcheck "$PWD"/script.sh
-nexit="$?"
+for file in "${shflist[@]}"; do
+  shellcheck "$PWD""$file"
+done
 
 #if [ $? -eq 0 ]; then
 #  echo "NEW BUILD is OK!"
 #else
 #  echo "NEW BUILD is WRONG!"
 #fi
+
+#------------#
+#    OLD     #
+#------------#
 
 #Check for prev commit
 git checkout ${rlist[1]}
