3dd5e1f79c4475ec008bb73b798f00167400dcac
Jan Macku <jamacku@redhat.com>
2020-01-30 15:31:02 +0100

    Work on new testing script

--- ci/check_pr.sh
+++ ci/check_pr.sh
@@ -8,6 +8,14 @@ git fetch
 git diff origin/"$TRAVIS_BRANCH"
 
 # get names of files from PR (excluding deleted files)
-git diff --name-only --diff-filter=AM HEAD..."$TRAVIS_BRANCH"
+git diff --name-only --diff-filter=AM HEAD..."$TRAVIS_BRANCH" pr-changes.txt
 
+# Find modified shell scripts
+readarray list_of_changes < "$PWD"/pr-changes.txt
+list_of_changed_scripts=()
+for file in "${list_of_changes[@]}"; do
+  is_it_script "$file" && list_of_changed_scripts+=("$file")
+done
 
+echo "changed files: "
+echo "${list_of_changed_scripts[@]}"
--- ci/functions.sh
+++ ci/functions.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 # Function to check whether input param is on list of shell scripts
-# $1 - <string> relative path to file
+# $1 - <string> absolute path to file
 is_it_script () {
   [ $# -eq 0 ] && return 1
-  # test
-  return 0
+  
+  readarray list_of_scripts < "$PWD"/.script-list.txt
+  echo "${list_of_scripts[@]}" | grep --silent "$1" && return 0
 }
 
