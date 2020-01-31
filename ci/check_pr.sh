a4c7c6290dbbee0a77ecc3f43ceb9178dbfcb794
Jan Macku <jamacku@redhat.com>
2020-01-31 15:01:20 +0100

    Lets find where we are

--- ci/check_pr.sh
+++ ci/check_pr.sh
@@ -2,11 +2,12 @@
 
 . ./functions.sh
 
-# https://stackoverflow.com/questions/41145041/list-files-modified-in-a-pull-request-within-travis
-git remote set-branches --add origin "$TRAVIS_BRNACH"
-git fetch
-git diff origin/"$TRAVIS_BRANCH"
+tree .
 
+echo ".."
+tree ../
+
+# https://stackoverflow.com/questions/41145041/list-files-modified-in-a-pull-request-within-travis
 # get names of files from PR (excluding deleted files)
 git diff --name-only --diff-filter=AM HEAD..."$TRAVIS_BRANCH" > ../pr-changes.txt
 
