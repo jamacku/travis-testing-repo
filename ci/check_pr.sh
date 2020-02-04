cb359d6139021b78120f313218c74f24d4a3c304
Jan Macku <jamacku@redhat.com>
2020-02-04 13:48:49 +0100

    exlude deleted files from testing

--- ci/check_pr.sh
+++ ci/check_pr.sh
@@ -4,7 +4,7 @@
 
 # https://medium.com/@joey_9999/how-to-only-lint-files-a-git-pull-request-modifies-3f02254ec5e0
 # get names of files from PR (excluding deleted files)
-git diff --name-only --diff-filter=b "$(git merge-base HEAD "$TRAVIS_BRANCH")" > ../pr-changes.txt
+git diff --name-only --diff-filter=db "$(git merge-base HEAD "$TRAVIS_BRANCH")" > ../pr-changes.txt
 
 echo "PR changes:"
 cat ../pr-changes.txt
@@ -23,14 +23,9 @@ shellcheck --format=gcc "${list_of_changed_scripts[@]}" > ../pr-shellcheck.txt
 
 cat ../pr-shellcheck.txt
 
-tree .
-
 git checkout "$TRAVIS_BRANCH"
 
 shellcheck --format=gcc "${list_of_changed_scripts[@]}" > ../merbr-shellcheck.txt
 
 cat ../merbr-shellcheck.txt
-
-tree .
-
 
