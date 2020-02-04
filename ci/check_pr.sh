b1330554ff286b89c36c387545874ba8a7ceae4c
Jan Macku <jamacku@redhat.com>
2020-02-04 16:31:27 +0100

    Cleaning

--- ci/check_pr.sh
+++ ci/check_pr.sh
@@ -2,12 +2,13 @@
 
 . ./ci/functions.sh
 
+# ------------ #
+#  FILE PATHS  #
+# ------------ #
+
 # https://medium.com/@joey_9999/how-to-only-lint-files-a-git-pull-request-modifies-3f02254ec5e0
 # get names of files from PR (excluding deleted files)
 git diff --name-only --diff-filter=db "$(git merge-base HEAD "$TRAVIS_BRANCH")" > ../pr-changes.txt
-
-echo "PR changes:"
-cat ../pr-changes.txt
 
 # Find modified shell scripts
 readarray list_of_changes < ../pr-changes.txt
@@ -17,41 +18,38 @@ for file in "${list_of_changes[@]}"; do
   is_it_script "$file" && list_of_changed_scripts+=("./${file//[$'\t\r\n ']}")
 done
 
-echo "changed files: "
-echo "${list_of_changed_scripts[@]}"
-
-# Shellcheck PR
+# ------------ #
+#  SHELLCHECK  #
+# ------------ #
 
 shellcheck --format=gcc "${list_of_changed_scripts[@]}" > ../pr-br-shellcheck.err
-
-# Shellcheck master
-
 git checkout "$TRAVIS_BRANCH"
-
 shellcheck --format=gcc "${list_of_changed_scripts[@]}" > ../dest-br-shellcheck.err
 
-# Validation
+# ------------ #
+#  VALIDATION  #
+# ------------ #
 
 exitstatus=0
 
 csdiff --fixed "../dest-br-shellcheck.err" "../pr-br-shellcheck.err" > ../fixes.log
 if [ "$(cat ../fixes.log | wc -l)" -ne 0 ]; then
-  echo "Fixed bugs since last version:" 
+  echo "Fixed bugs:" 
   csgrep ../fixes.log
   echo "------------"
 else
-  echo "No Fixes since last version!"
+  echo "No Fixes!"
   echo "------------"
 fi
 
 csdiff --fixed "../pr-br-shellcheck.err" "../dest-br-shellcheck.err" > ../bugs.log
 if [ "$(cat ../bugs.log | wc -l)" -ne 0 ]; then
-  echo "Added bugs since last version:" 
+  echo "Added bugs, NEED INSPECTION:" 
   csgrep ../bugs.log
   echo "------------"
   exitstatus=1
 else
-  echo "No changes since last version!" 
+  echo "No bugs added Yay!" 
   echo "------------"
   exitstatus=0
 fi
