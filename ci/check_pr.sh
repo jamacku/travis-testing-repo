55d558e0d92ae38459aede50632441c5af62f856
Jan Macku <jamacku@redhat.com>
2020-02-04 13:35:24 +0100

    add debug outputs

--- ci/check_pr.sh
+++ ci/check_pr.sh
@@ -4,6 +4,8 @@
 
 tree .
 
+echo "$TRAVIS_BRANCH"
+
 # https://medium.com/@joey_9999/how-to-only-lint-files-a-git-pull-request-modifies-3f02254ec5e0
 # get names of files from PR (excluding deleted files)
 git diff --name-only --diff-filter=b $(git merge-base HEAD $TRAVIS_BRANCH) > ../pr-changes.txt
@@ -25,11 +27,14 @@ shellcheck --format=gcc "${list_of_changed_scripts[@]}" > ../pr-shellcheck.txt
 
 cat ../pr-shellcheck.txt
 
+tree .
 
 git checkout master
 
 shellcheck --format=gcc "${list_of_changed_scripts[@]}" > ../master-shellcheck.txt
 
 cat ../master-shellcheck.txt
+
+tree .
 
 
